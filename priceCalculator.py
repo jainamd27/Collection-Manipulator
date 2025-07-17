9d7437436308d8d9e720bed1147018c1804c0c36
Jainam doshi <70855989+jainamd27@users.noreply.github.com>
2025-07-17 17:49:33 +0530

    Delete priceCalculator.py

--- priceCalculator.py
+++
@@ -1,9 +0,0 @@
-unit1 = 100
-unit2 = 100
-unit3 = 100
-unit4 = 200
-
-totalBill = (unit1*0.50)+(unit2*0.75)+(unit3*1.25)+(unit4*1.75)
-
-totalAmountToPay = totalBill + ((totalBill*20)/100)
-print("Total Amount to pay is: ", totalAmountToPay)
